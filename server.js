d344049a9ea10d69f90c919253e4f5a55517dd41
Dandy <dandy.chen@buyandship.com.tw>
2020-04-15 18:42:41 +0800

    change deploy way

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var proxy = require('http-proxy-middleware');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(__dirname));
+app.use(serveStatic(__dirname + '/dist'));
 app.use(
   '/api',
   proxy({ 
